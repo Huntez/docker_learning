4896fc4731e4293eb5393b8fa28f705fe70cb8be
Huntez <osigor5464@gmail.com>
2023-12-08 10:24:31 +0000

    create dockerfile for node and python app

---
+++ python-app/main.py
@@ -0,0 +1,8 @@
+import calendar
+
+print("hello in calendar\n")
+year = int(input('Enter year : '))
+month = int(input('Enter a mount : '))
+
+print('\n', calendar.month(year, month))
+print('good luck!')
